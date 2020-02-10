f16c931a2d36084703296d895b3b5119b898ee68
RyanH <ryan.hendricks@baruchmail.cuny.edu>
2020-02-10 14:47:25 -0500

    Update build pipeline

--- quasar.conf.js
+++ quasar.conf.js
@@ -63,7 +63,7 @@ module.exports = function (ctx) {
       vueRouterMode: 'history', // available values: 'hash', 'history'
       showProgress: true,
       gzip: true,
-      analyze: true,
+      analyze: false,
       // Options below are automatically set depending on the env, set them if you want to override
       preloadChunks: true,
       extractCSS: true,
