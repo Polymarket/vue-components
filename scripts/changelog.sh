e11d17dac7ae743dd1516ce3c8d889cc0efa761b
RyanH <ryan.hendricks@baruchmail.cuny.edu>
2020-02-10 14:55:05 -0500

    Update docs and changelog

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator -u TokenUnion -p union-marketplace-icf-grant --token $GHTOKEN
+docker run -it --rm -v "$(pwd)":/usr/local/src/your-app ferrarimarco/github-changelog-generator -u TokenUnion -p vue-components --token $GHTOKEN
